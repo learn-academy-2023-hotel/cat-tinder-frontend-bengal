3f21b1ec8c8714fef15ab8482f424ca591997041
anthonyholt <ant.scott227@gmail.com>
2023-12-08 15:05:08 -0800

    frontend complete

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+  <div className="header">WildCat Tinder</div>
+  )
+};
+
+export default Header;
---
+++ src/mockCats.js
@@ -0,0 +1,26 @@
+const mockCats = [
+    {
+        id: 1,
+        name: 'Blackwell',
+        age: 4,
+        enjoy: 'long walks on the beach',
+        image: "https://twitter.com/susantananda3/status/1330866962675625988/photo/1"
+    },
+    {
+        id: 2,
+        name: 'Ash',
+        age: 6,
+        enjoy: 'eating catnip',
+        image: 'https://images.unsplash.com/photo-1513617753818-29509cdfdd9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        id: 3,
+        name: 'Badger',
+        age: 5,
+        enjoy: 'a nice long nap',
+        image: 'https://as2.ftcdn.net/v2/jpg/05/87/92/55/1000_F_587925530_VUYOLH6hI9RALjteSJo4T7gqYgHft9C2.jpg'
+    }
+   
+]
+
+export default mockCats
