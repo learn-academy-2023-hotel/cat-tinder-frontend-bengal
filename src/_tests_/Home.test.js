592bdc17ea665cdfd8de763eb596fe7163bf96f6
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-12-11 14:14:08 -0800

    test coverage complete

--- src/_tests_/Home.test.js
+++ src/_tests_/Home.test.js
@@ -1,9 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 
 describe("<Home />", () => {
+  const renderHome = () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+  }
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    render(<Home />, div);
+    renderHome()
   });
+  it("checks for the first image tag and all of its props", () => {
+    renderHome()
+  const logo = screen.getByRole("img")
+  expect (logo).toHaveAttribute("id", "1")
+  expect (logo).toHaveAttribute("src", "https://images.unsplash.com/photo-1623645481161-0d8160281cbf?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+  expect (logo).toHaveAttribute("alt", "cat image")
+  expect (logo).toHaveAttribute("width", "500")
 });
+});
+
+
+// const div = document.createElement("div");
+//     render(<Home />, div);
--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
 
-const Catindex = () => {
-return (
-<div>
-Catindex
-</div>
-)
-}
+const Catindex = ({ Cats }) => {
+  return (
+    <div>
+      {Cats?.map((cat, index) => {
+        return (
+          <Card
+            style={{
+              width: "18rem",
+            }}
+            key={index}
+          >
+            <img alt="big cat profile pic" src={cat.image} />
+            <CardBody>
+              <CardTitle tag="h5">{cat.name}</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                {cat.age}
+              </CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card‘s content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Catindex
+export default Catindex;
