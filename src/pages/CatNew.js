94ee137ca47ef2e63548f09519955ab217a7c77d
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-12-12 15:54:48 -0800

    create functionality complete

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
-const CatNew = () => {
-return (
-<div>
-CatNew
-</div>
-)
-}
+const CatNew = ({ createCat }) => {
+    const navigate = useNavigate()
+  const [newCat, setNewCat] = useState({
+    name: "",
+    age: "",
+    enjoy: "",
+    image: "",
+  });
 
-export default CatNew
+  const handleChange = (e) => {
+    setNewCat({ ...newCat, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    createCat(newCat);
+    navigate("/catindex")
+  };
+
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input
+            id="name"
+            name="name"
+            placeholder="Enter Name"
+            type="text"
+            onChange={handleChange}
+            value={newCat.name}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="age">Age</Label>
+          <Input id="age" name="age" placeholder="Enter age" type="text" onChange={handleChange}
+            value={newCat.age}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="enjoy">Enjoy</Label>
+          <Input id="enjoy" name="enjoy" placeholder="enjoy..." type="email" onChange={handleChange}
+            value={newCat.enjoy}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="image">Image</Label>
+          <Input id="image" name="image" placeholder="image.png" type="text" onChange={handleChange}
+            value={newCat.image}/>
+        </FormGroup>
+      </Form>
+      <Button onClick={handleSubmit} name="submit">
+        Submit
+      </Button>
+    </>
+  );
+};
+
+export default CatNew;
