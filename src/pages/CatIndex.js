ffac4cd0202f179b2fb6507939d50340b290997b
anthonyholt <ant.scott227@gmail.com>
2023-12-13 19:01:30 -0800

    new styles

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -5,16 +5,16 @@ import { Button } from "reactstrap";
 
 const Catindex = ({ cats }) => {
   return (
-    <div>
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center"}}>
       {cats?.map((cat, index) => {
         return (
           <Card
             style={{
-              width: "18rem",
+              width: "18rem", height: "300px", margin: "10px", 
             }}
             key={index}
           >
-            <img alt="big cat profile pic" src={cat.image} />
+            <img alt="big cat profile pic" src={cat.image} style={{ objectFit: "cover" }} />
             <CardBody>
               <CardTitle tag="h5">{cat.name}</CardTitle>
               <CardSubtitle className="mb-2 text-muted" tag="h6">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,16 @@ import React from "react";
 const Home = () => {
 //   const [cats, setCats] = useState(mockCats);
   return (
-    <main className="center">
-      <img 
-      id="1"
-        src='https://images.unsplash.com/photo-1623645481161-0d8160281cbf?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        alt="cat image"
-        width="500"
-      />
-    </main>
+    // <main className="home-cat">
+    //   <img 
+    //   id="1"
+    //     src='https://images.unsplash.com/photo-1623645481161-0d8160281cbf?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    //     alt="cat image"
+    //     height="300"
+    //     width="300"
+    //   />
+    // </main>
+      <div className="home-cat"></div>
   );
 };
 export default Home;
